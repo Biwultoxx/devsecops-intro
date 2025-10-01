1bd60185ed268bed2202cbf59fc535d5669d706a
aingmeong <bG9yZW0gaXBzdW0gYWhoaCBhaGhoaCA=@Z21haWwK.com>
2025-10-01 14:48:54 +0700

    update test_greeting

--- greeting.py
+++ greeting.py
@@ -1,4 +1,5 @@
 def greeting(name):
-    return f"Hello {name}, welcome to DevSecOps!"
+    return f"Hello {name},welcome to DevSecOps!"  # tanpa spasi
+
 if __name__ == "__main__":
     print(greeting("Mahasiswa"))
